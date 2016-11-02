05ff51e7933ee2bf5f07148c5b841f5108f071cf
ZT0526 <zhaotian@ZTdeMacBook-Pro.local>
2016-11-02 10:34:44 +0800

    first commit

---
+++ ZTAvPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZTAvPlayer
+//
+//  Created by 赵天 on 16/3/18.
+//  Copyright © 2016年 赵天. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
